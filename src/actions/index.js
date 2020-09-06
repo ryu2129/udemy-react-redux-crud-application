a43234ab6476befa4f73cedef48c17430f82ccd9
Ryunosuke Abe <ryu212920@gmail.com>
2020-09-07 00:28:52 +0900

    fix redux

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,10 @@
-const INCREMENT = 'INCREMENT'
-const DECREMENT = 'DECREMENT'
+export const INCREMENT = 'INCREMENT'
+export const DECREMENT = 'DECREMENT'
 
-export const increment = () => {{
+export const increment = () => ({
   type: INCREMENT
-}}
+})
 
-export const decrement = () => {{
+export const decrement = () => ({
   type: DECREMENT
-}}
+})
--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux'
 
 import { increment, decrement } from '../actions'
 
-class App extends Component {
+class EventsIndex extends Component {
   render() {
     const props = this.props
 
@@ -20,6 +20,5 @@ const mapStateToProps = state => ({ value: state.count.value })
 
 const mapDispatchToProps = ({ increment, decrement })
 
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default connect(mapStateToProps, mapDispatchToProps)(EventsIndex)
 
-export default App;
